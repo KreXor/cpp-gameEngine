95bea1f836b22d7b5c54362d6108fee6384d613b
Daniel Johansson <krexor@gmail.com>
2015-09-14 22:29:00 +0200

    added json error handling for map and animation

--- src/Animation/AnimationReader.cpp
+++ src/Animation/AnimationReader.cpp
@@ -43,43 +43,95 @@ Sprite* AnimationReader::LoadAnimations(string filename, GameEngine* game)
     Document document;
     document.Parse(buffer);
 
-    string path = document["sprite_set"].GetString();
-
-    const Value& a = document["animations"];
-    assert(a.IsArray());
-
-
-    for (SizeType i = 0; i < a.Size(); i++)
-    {
-        frameRate = a[i]["framerate"].GetInt();
-        frameCount = a[i]["framecount"].GetInt();
-
-        animation = new Animation();
-        if(animation == NULL)
-        {
-            delete animation;
-        }
-
-        animation->Init(frameCount, frameRate, path, game);
-
-        for (SizeType j = 0; j < a[i]["frames"].Size(); j++)
-        {
-            frame = a[i]["frames"][j]["frame"].GetInt();
-            x = a[i]["frames"][j]["pos_x"].GetInt();
-            y = a[i]["frames"][j]["pos_y"].GetInt();
-            w = a[i]["frames"][j]["width"].GetInt();
-            h = a[i]["frames"][j]["height"].GetInt();
-
-            animation->SetFrame(frame, x, y, w, h);
-        }
-
-        this->sprite->AddAnimation(animation, animationCount);
-        animationCount++;
-    }
-
     //Cleanup!
     delete[] buffer;
 
+    if(document.IsObject())
+    {
+        if(!document.HasMember("sprite_set"))
+        {
+            cout << "failed to animation (" << filename << ")! has no member tile_set\n";
+            return this->sprite;
+        }
+        else if(!document["sprite_set"].IsString())
+        {
+            cout << "failed to animation (" << filename << ")! member tile_set is not a string\n";
+            return this->sprite;
+        }
+
+         if(!document.HasMember("animations"))
+        {
+            cout << "failed to animation (" << filename << ")! has no member animations\n";
+            return this->sprite;
+        }
+        else if(!document["animations"].IsArray())
+        {
+            cout << "failed to animation (" << filename << ")! member animations is not an array\n";
+            return this->sprite;
+        }
+
+        string path = document["sprite_set"].GetString();
+
+        const Value& a = document["animations"];
+
+
+
+        for (SizeType i = 0; i < a.Size(); i++)
+        {
+            if(!a[i].HasMember("framerate"))
+            {
+                cout << "failed to animation (" << filename << ")! has no member framerate\n";
+                return this->sprite;
+            }
+            else if(!a[i]["framerate"].IsInt())
+            {
+                cout << "failed to animation (" << filename << ")! member framerate is not an int\n";
+                return this->sprite;
+            }
+
+            if(!a[i].HasMember("framecount"))
+            {
+                cout << "failed to animation (" << filename << ")! has no member framecount\n";
+                return this->sprite;
+            }
+            else if(!a[i]["framecount"].IsInt())
+            {
+                cout << "failed to animation (" << filename << ")! member framecount is not an int\n";
+                return this->sprite;
+            }
+
+            frameRate = a[i]["framerate"].GetInt();
+            frameCount = a[i]["framecount"].GetInt();
+
+            animation = new Animation();
+            if(animation == NULL)
+            {
+                delete animation;
+            }
+
+            animation->Init(frameCount, frameRate, path, game);
+
+            for (SizeType j = 0; j < a[i]["frames"].Size(); j++)
+            {
+                frame = a[i]["frames"][j]["frame"].GetInt();
+                x = a[i]["frames"][j]["pos_x"].GetInt();
+                y = a[i]["frames"][j]["pos_y"].GetInt();
+                w = a[i]["frames"][j]["width"].GetInt();
+                h = a[i]["frames"][j]["height"].GetInt();
+
+                animation->SetFrame(frame, x, y, w, h);
+            }
+
+            this->sprite->AddAnimation(animation, animationCount);
+            animationCount++;
+        }
+
+
+    }
+    else
+    {
+        cout << "failed to animation (" << filename << ")! not a correct json file\n";
+    }
 	return this->sprite;
 }
 
--- src/Map/Map.cpp
+++ src/Map/Map.cpp
@@ -37,41 +37,104 @@ void Map::readMapData(GameEngine* game)
     Document document;
     document.Parse(buffer);
 
-    Tile tile;
-	int count = 0;
-
-    string tile_set = document["tile_set"].GetString();
-    int tile_size = document["tile_size"].GetInt();
-    this->tilesCount = document["tile_count"].GetInt();
-    this->tilesImage[0] = Load_image(tile_set, game);
-
-    const Value& a = document["grid"];
-   // assert(a.IsArray());
-
-
-    mapTilePosition = new Tile[this->tilesCount];
-
-    for (SizeType i = 0; i < a.Size(); i++)
-    {
-       // assert(a[i].IsArray());
-        for (SizeType j = 0; j < a[i].Size(); j++)
-        {
-            int type_id = a[i][j]["type"].GetInt();
-            tile.worldPosition.x = j*tile_size;
-            tile.worldPosition.y = i*tile_size;
-            tile.imagePosition.x = document["type_legend"][type_id]["image_x"].GetInt();
-            tile.imagePosition.y = document["type_legend"][type_id]["image_y"].GetInt();
-            tile.type = 0;
-            tile.layer = 0;
-            tile.boundingBoxType = 0;
-            tile.t_size = tile_size;
-            mapTilePosition[count] = tile;
-            count++;
-        }
-    }
-
-    //Cleanup!
+    //clean up buffer
     delete[] buffer;
+
+    if(document.IsObject())
+    {
+        Tile tile;
+        int count = 0;
+
+
+
+        //Check json objects
+        if(!document.HasMember("tile_set"))
+        {
+            cout << "failed to load map! member: tile_set not found\n";
+            return;
+        }
+        else if(!document["tile_set"].IsString())
+        {
+            cout << "failed to load map! member: tile_set is not a string\n";
+            return;
+        }
+
+        if(!document.HasMember("tile_size"))
+        {
+            cout << "failed to load map! member: tile_size not found\n";
+            return;
+        }
+        else if(!document["tile_size"].IsInt())
+        {
+            cout << "failed to load map! member: tile_size is not an integer\n";
+            return;
+        }
+
+        if(!document.HasMember("tile_count"))
+        {
+            cout << "failed to load map! member: tile_count not found\n";
+            return;
+        }
+        else if(!document["tile_count"].IsInt())
+        {
+            cout << "failed to load map! member: tile_count is not an integer\n";
+            return;
+        }
+
+        if(!document.HasMember("grid"))
+        {
+            cout << "failed to load map! member: grid not found\n";
+            return;
+        }
+        else if(!document["grid"].IsArray())
+        {
+            cout << "failed to load map! member: grid is not an array\n";
+            return;
+        }
+
+        //load json objects
+        string tile_set = document["tile_set"].GetString();
+        int tile_size = document["tile_size"].GetInt();
+        this->tilesCount = document["tile_count"].GetInt();
+        this->tilesImage[0] = Load_image(tile_set, game);
+
+        const Value& a = document["grid"];
+       // assert(a.IsArray());
+
+
+        mapTilePosition = new Tile[this->tilesCount];
+
+        for (SizeType i = 0; i < a.Size(); i++)
+        {
+            if(a[i].IsArray())
+            {
+                for (SizeType j = 0; j < a[i].Size(); j++)
+                {
+                    int type_id = a[i][j]["type"].GetInt();
+                    tile.worldPosition.x = j*tile_size;
+                    tile.worldPosition.y = i*tile_size;
+                    tile.imagePosition.x = document["type_legend"][type_id]["image_x"].GetInt();
+                    tile.imagePosition.y = document["type_legend"][type_id]["image_y"].GetInt();
+                    tile.type = 0;
+                    tile.layer = 0;
+                    tile.boundingBoxType = 0;
+                    tile.t_size = tile_size;
+                    mapTilePosition[count] = tile;
+                    count++;
+                }
+            }
+            else
+            {
+                cout << "failed to load map! could not read tile list\n";
+                return;
+            }
+        }
+
+    }
+    else
+    {
+        cout << "failed to load map: not a correct json file\n";
+    }
 
 }
 void Map::Draw(GameEngine* game, float x_offset, float y_offset)
